9677d12ffbf0c3d21c435105bd04b9d29737ed6a
preetisarmal <pk0232832@gmail.com>
2024-07-09 22:10:36 +0530

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5] = {10, 20, 30, 40, 50};
+    
+    // Printing addresses of all elements in the array
+    printf("Addresses of elements in the array:\n");
+    for (int i = 0; i < 5; ++i) 
+    {
+        printf("Address of arr[%d]: %u\n", i, &arr[i]);
+    }
+    
+    return 0;
+}
